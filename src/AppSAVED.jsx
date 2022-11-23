e0de0fb472359304c6f9ba6e2b13e29ec52f6f7a
Gillian Brousse <gillian.brousse@neuf.fr>
2022-11-23 23:04:04 +0100

    working on axios imports

--- src/AppSAVED.jsx
+++ src/AppSAVED.jsx
@@ -19,17 +19,13 @@ import Univers from "./pages/Univers";
 import Search from "./pages/Search";
 
 /***DATAS***/
-/*
 import Data1 from "./data/data.json";
 import Data2 from "./data/dataExt.json";
 import Data3 from "./data/dataExt2.json";
-*/
 
 /***APP***/
 export default function App() {
   /*Get datas*/
-
-  /*
   const [allDatas, setAllDatas] = useState({});
   const [allDatas2, setAllDatas2] = useState({});
   const [allDatas3, setAllDatas3] = useState({});
@@ -42,7 +38,6 @@ export default function App() {
     }
     fetchDatas();
   }, []);
-  */
 
   /****************IF NEEDED******************************/
   /*Get all books tags*/
@@ -55,7 +50,51 @@ export default function App() {
       {/*BACKGROUND ANIMATION*/}
       {DustAnim()}
       {MainAnim()}
-      <Header data1={Data1} data2={Data2} />     
+      <Header data1={Data1} data2={Data2} />
+      <div className="app">
+        {/*ROUTES*/}
+        <Switch>
+          {/*HOMEPAGE*/}
+          <Route exact path="/accueil">
+            <Homepage data1={Data1} data2={Data2} data3={Data3} />
+          </Route>
+          {/*CREATEURS*/}
+          <Route exact path="/createurs">
+            <Createurs data1={Data1} data2={Data2} />
+          </Route>
+          {/*UNIVERS HOMEPAGE*/}
+          <Route exact path="/nos-univers">
+            <Univers data1={Data1} data2={Data2} />
+          </Route>
+          {/*UNIVERS*/}
+          <Route
+            exact
+            path={[
+              "/memoria",
+              "/futuria",
+              "/onyria",
+              "/terra-urbana",
+              "/rouge-d-absinthe",
+              "/l-antre-de-la-folie",
+              "/fleur-d-absinthe",
+              "/chrysalis",
+              "/pousse-d-absinthe",
+              "/hors-collection",
+            ]}
+          >
+            <ThisUniverse data1={Data1} data2={Data2} data3={Data3} />
+          </Route>
+          {/*SEARCH*/}
+          <Route path="/recherche">
+            <Search data1={Data1} data2={Data2} data3={Data3} />
+          </Route>
+          {/*REDIRECT*/}
+          <Route>
+            <Redirect to="/accueil" />
+          </Route>
+        </Switch>
+      </div>
+      <Footer data1={Data1} data2={Data2} />
     </Router>
   );
 }
