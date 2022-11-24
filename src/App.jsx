b7b54c08ca785bd6286df25c93dafe08558a619e
Gillian Brousse <gillian.brousse@neuf.fr>
2022-11-24 01:23:50 +0100

    test distant json modification 6

--- src/App.jsx
+++ src/App.jsx
@@ -42,9 +42,9 @@ export default function App() {
   /*let Json1 = "https://raw.githubusercontent.com/GilBrou/nda/master/data.json";*/
   let Json1 = "https://raw.githubusercontent.com/NoirDAbsinthe/NDAAPI/master/data.json";
   let Json2 =
-    "https://raw.githubusercontent.com/NoirDAbsinthe/NDAAPI/master/dataSite.json";
+    "https://raw.githubusercontent.com/NoirDAbsinthe/NDAAPI/master/dataExt.json";
   let Json3 =
-        "https://raw.githubusercontent.com/NoirDAbsinthe/NDAAPI/master/dataBooks.json";
+        "https://raw.githubusercontent.com/NoirDAbsinthe/NDAAPI/master/dataExt2.json";
 
 
   const requestOne = axios.get(Json1);
