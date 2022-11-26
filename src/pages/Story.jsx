4f835558b4461749f94e53f7b64446a1dc19f992
Gillian Brousse <gillian.brousse@neuf.fr>
2022-11-26 19:56:53 +0100

    working on Short stories page 3.5/5

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -58,153 +58,50 @@ export default function Story() {
     nextPage(data);
   }
 
-  /*StoryTelling*/
-  function StoryTelling(data) {
-    /*console.log(data)*/
-    /*Check if this is the first page*/
-    if (page == 0) {
-      return (
-        <div className="storyText">
-          <h1>{data.Titre}</h1>
-          <h2 className="auteur">{"par " + data.Auteur}</h2>
-          <button
-            className="startButton"
-            onClick={() => {
-              firstClick(data);
-            }}
-          >
-            Commencer la lecture
-          </button>
-        </div>
-      );
-    }
-    /*All story pages*/
-    if (page >= 1 && page < 21) {
-      let refresh = document.getElementById("refresh");
-      refresh.style.visibility = "visible";
-      return (
-        <div className="storyText ">
-          {/*Page number*/}
-          <p className="pageNum">{"Page " + page + " / 21"}</p>
+  /*DOM*/
+  return (
+    <div className="story">
+      <div className="storyWrap">
+        {DustAnim()}
+        <div className="top">
+          <div className="left0">
+            {/*goBack button*/}
+            <div className="goBack">
+              <button
+                id="goBack"
+                className="goBack"
+                onClick={() => {
+                  window.location = "/nouvelles";
+                }}
+              >
+                {" "}
+                <i className="fa fa-arrow-left" aria-hidden="true"></i>
+              </button>
+            </div>
+            {/*Refresh button*/}
+            <div className="refresh">
+              <button
+                id="refresh"
+                className="refresh"
+                onClick={() => {
+                  location.reload();
+                }}
+              >
+                {" "}
+                <i className="fa fa-refresh" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
 
-          {(() => {
-            if (page > 1) {
-              /*previous button*/
-              return (
-                <button
-                  className="previous"
-                  onClick={() => {
-                    previousPage(data);
-                  }}
-                >
-                  <i className="fa fa-chevron-left" aria-hidden="true"></i>
-                </button>
-              );
-            }
-          })()}
-          {(() => {
-            if (page == 1) {
-              /*Dynamic display of every paragraph from Json according to page*/
-              return data.P1.map((P, i) => (
-                <h2 className="firstT" key={"P" + i}>
-                  {P}
-                </h2>
-              ));
-            }
-            if (page == 2) {
-              return data.P2.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 3) {
-              return data.P3.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 4) {
-              return data.P4.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 5) {
-              return data.P5.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 6) {
-              return data.P6.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 7) {
-              return data.P7.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 8) {
-              return data.P8.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 9) {
-              return data.P9.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 10) {
-              return data.P10.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 11) {
-              return data.P11.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 12) {
-              return data.P12.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 13) {
-              return data.P13.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 14) {
-              return data.P14.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 15) {
-              return data.P15.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 16) {
-              return data.P16.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 17) {
-              return data.P17.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 18) {
-              return data.P18.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 19) {
-              return data.P19.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-            if (page == 20) {
-              return data.P20.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
-            }
-          })()}
-          {/*Next button*/}
-          <button
-            className="next"
-            onClick={() => {
-              nextPage(data);
-            }}
-          >
-            <i className="fa fa-chevron-right" aria-hidden="true"></i>
-          </button>
+          {/*all texts part*/}
+          {StoryTelling(data)}
         </div>
-      );
-    }
-    {
-      /*Check if this is the last page*/
-    }
-    if (page == 21) {
-      return (
-        <div className="storyText ">
-          {/*Previous button*/}
-          <button
-            className="previous"
-            onClick={() => {
-              previousPage(data);
-            }}
-          >
-            <i className="fa fa-chevron-left" aria-hidden="true"></i>
-          </button>
-          {/*Page number*/}
-          <p className="pageNum">{"Page " + page + " / 21"}</p>
-          {data.P21.map((P, i) => (
-            /*Lasts paragraphs*/
-            <h2 key={"P" + i}>{P}</h2>
-          ))}
+
+        {/*Audio player*/}
+        <div className="bottom">
           {/*End buttons*/}
-          {
-            /*Author button*/
+          <div className="buttonWrap">
+            {/*Author button*/}
             <button
               className="fin1"
               onClick={() => {
@@ -213,10 +110,8 @@ export default function Story() {
             >
               {"Découvrir d'autres textes de " + data.Auteur}
             </button>
-          }
 
-          {
-            /*Back to short stories page*/
+            {/*Back to short stories page*/}
             <button
               className="fin2"
               onClick={() => {
@@ -225,58 +120,184 @@ export default function Story() {
             >
               Retour à l'accueil des nouvelles
             </button>
-          }
-        </div>
-      );
-    }
-  }
+          </div>
 
-  /*DOM*/
-  return (
-    <div className="story">
-      <div className="storyWrap">
-        {DustAnim()}
-        {/*Refresh button*/}
-        <div className="refresh">
-          <button
-            id="refresh"
-            className="refresh"
-            onClick={() => {
-              location.reload();
-            }}
-          >
-            {" "}
-            <i className="fa fa-refresh" aria-hidden="true"></i>
-          </button>
-        </div>
-        {/*goBack button*/}
-        <div className="goBack">
-          <button
-            id="goBack"
-            className="goBack"
-            onClick={() => {
-              window.location = "/nouvelles";
-            }}
-          >
-            {" "}
-            <i className="fa fa-arrow-left" aria-hidden="true"></i>
-          </button>
-        </div>
-        {/*all texts part*/}
-        {StoryTelling(data)}
-        {/*Audio player*/}
-        <div className="audioWrap">
-          {/*radio antenna*/}
-          <div className="antenna"></div>
           <div id="audio">
+            {/*radio antenna*/}
+            <div className="antenna"></div>
             {/*Woofers styling*/}
             <div className="Radio Radio1"></div>
             <div className="Radio Radio2"></div>
             <p>Ambiance sonore</p>
-            <audio id="audioP" src={audio} autoPlay controls loop></audio>
+            <audio id="audioP" src={audio} /*autoPlay*/ controls loop></audio>
           </div>
         </div>
       </div>
     </div>
   );
+
+  /*StoryTelling*/
+  function StoryTelling(data) {
+    /*console.log(data)*/
+    /*Check if this is the first page*/
+    if (page == 0) {
+      return (
+        <div className="middle">
+          <div className="storyText">
+            <h1>{data.Titre}</h1>
+            <h2 className="auteur">{"par " + data.Auteur}</h2>
+            <button
+              className="startButton"
+              onClick={() => {
+                firstClick(data);
+              }}
+            >
+              Commencer la lecture
+            </button>
+          </div>
+        </div>
+      );
+    }
+    /*All story pages*/
+    if (page >= 1 && page < 21) {
+      let refresh = document.getElementById("refresh");
+      refresh.style.visibility = "visible";
+      return (
+        <div>
+          <div className="left">
+            {(() => {
+              if (page > 1) {
+                /*previous button*/
+                return (
+                  <button
+                    className="previous"
+                    onClick={() => {
+                      previousPage(data);
+                    }}
+                  >
+                    <i className="fa fa-chevron-left" aria-hidden="true"></i>
+                  </button>
+                );
+              }
+            })()}
+          </div>
+          <div className="storyText ">
+            <div className="middle">
+              {/*Page number*/}
+              <p className="pageNum">{"Page " + page + " / 21"}</p>
+              {(() => {
+                if (page == 1) {
+                  /*Dynamic display of every paragraph from Json according to page*/
+                  return data.P1.map((P, i) => (
+                    <h2 className="firstT" key={"P" + i}>
+                      {P}
+                    </h2>
+                  ));
+                }
+                if (page == 2) {
+                  return data.P2.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 3) {
+                  return data.P3.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 4) {
+                  return data.P4.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 5) {
+                  return data.P5.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 6) {
+                  return data.P6.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 7) {
+                  return data.P7.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 8) {
+                  return data.P8.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 9) {
+                  return data.P9.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 10) {
+                  return data.P10.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 11) {
+                  return data.P11.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 12) {
+                  return data.P12.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 13) {
+                  return data.P13.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 14) {
+                  return data.P14.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 15) {
+                  return data.P15.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 16) {
+                  return data.P16.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 17) {
+                  return data.P17.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 18) {
+                  return data.P18.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 19) {
+                  return data.P19.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+                if (page == 20) {
+                  return data.P20.map((P, i) => <h2 key={"P" + i}>{P}</h2>);
+                }
+              })()}
+            </div>
+            {/*Next button doestn hide*/}
+
+            <div className="right">
+              {/*Next button*/}
+              <button
+                className="next"
+                onClick={() => {
+                  nextPage(data);
+                }}
+              >
+                <i className="fa fa-chevron-right" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    {
+      /*Check if this is the last page*/
+    }
+    if (page == 21) {
+      return (
+        <div className="middle">
+          {/*Page number*/}
+          <p className="pageNum">{"Page " + page + " / 21"}</p>
+          <div className="left">
+            {/*Previous button*/}
+            <button
+              className="previous"
+              onClick={() => {
+                previousPage(data);
+              }}
+            >
+              <i className="fa fa-chevron-left" aria-hidden="true"></i>
+            </button>
+          </div>
+          <div className="storyText ">
+            {data.P21.map((P, i) => (
+              /*Lasts paragraphs*/
+              <h2 key={"P" + i}>{P}</h2>
+            ))}
+          </div>
+          }
+        </div>
+      );
+    }
+  }
 }
